6d96612e1e1ae428ff6ac61b9e576dfb665c6ed0
mehmet <mhmtranas@gmail.com>
2017-08-07 18:06:36 +0300

    CreatedModel

---
+++ PsikoterapsitlerBurada/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PsikoterapsitlerBurada.Models
+{
+    public class Question
+    {
+        public Question()
+        {
+            AskedToWhom = new HashSet<ApplicationUser>();
+        }
+        public int Id { get; set; }
+        [Required]
+        public string QuestionText { get; set; }
+        [Required]
+        public ApplicationUser WhoAsked { get; set; }
+        public ICollection<ApplicationUser> AskedToWhom { get; set; }
+        public DateTime? DateTime { get; set; }
+        [Required]
+        public Category Category { get; set; }
+    }
+}
