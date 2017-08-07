884f43ba5c41016e558d52c049c40e3b12a9797e
mehmet <mhmtranas@gmail.com>
2017-08-07 18:43:37 +0300

    Added Question View Model

---
+++ PsikoterapsitlerBurada/Models/QuestionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace PsikoterapsitlerBurada.Models
+{
+    public class QuestionViewModel
+    {
+        [DisplayName("Soru")]
+        public string QuestionText { get; set; }
+        public ApplicationUser WhoAsked { get; set; }
+        [DisplayName("Kimlere Sormak İstiyorsun?")]
+        public IEnumerable<ApplicationUser> AskedToWhom { get; set; }
+        public DateTime? DateTime { get; set; }
+        [DisplayName("Kategori")]
+        public Category Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
