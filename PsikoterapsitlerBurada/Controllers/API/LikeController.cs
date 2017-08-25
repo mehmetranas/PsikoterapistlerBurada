b8c22ab00002d13ed492ff1753f46596a9b75c96
mehmet <mhmtranas@gmail.com>
2017-08-25 12:14:32 +0300

    Fixed own like problem

--- PsikoterapsitlerBurada/Controllers/API/LikeController.cs
+++ PsikoterapsitlerBurada/Controllers/API/LikeController.cs
@@ -23,19 +23,16 @@ namespace PsikoterapsitlerBurada.Controllers.API
             var userId = User.Identity.GetUserId();
             var user = _contex.Users.SingleOrDefault(u => u.Id == userId);
             var answer = _contex.Answers.Include(a => a.User).SingleOrDefault(a => a.Id == id);
-            if (answer == null || user == null) return BadRequest();
+            if (answer == null || user == null || answer.User.Id == userId) return BadRequest();
 
             answer.Likes.Add(user);
-
-            var userForNotify = answer.User;
-
             var notification = new Notification()
             {
                 NotificationType = NotificationType.Like,
                 UserLike = user
             };
 
-            userForNotify.Notify(notification);
+            answer.User.Notify(notification);
 
             _contex.SaveChanges();
 
